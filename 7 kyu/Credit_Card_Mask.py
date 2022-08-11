15b61c92855228ec65ad88068e2cc9c53d2b6ecb
dmbiee <beleyhha@gmail.com>
2022-08-12 01:46:37 +0300

    Cleaned up the code a bit

--- 7 kyu/Credit_Card_Mask.py
+++ 7 kyu/Credit_Card_Mask.py
@@ -6,11 +6,3 @@ def maskify(cc):
         else:
             result += l
     return result[::-1]
-    
-    
-    
-    
-    # return ''.join( cc.replace(l, "#") for l in cc[::-1] if len(cc)>4)
-    # return "#"*(len(cc)-4) + cc[-4:]
-
-print(maskify('SF$SDfgsd2eA'))
